2a9576da971df1a165db1555c8a1caf1facbeaca
Krishna Baheti (Roll no. : U24AI133) <u24ai133@aid.svnit.ac.in>
2024-11-26 19:20:22 +0530

    Ques_60.c

--- Ques_60.c
+++ Ques_60.c
@@ -5,41 +5,37 @@
 
 // SELECTION SORT
 
-
 #include<stdio.h>
-
-void selectionSort(int arr[], int N) {
-    for (int i = 0; i < N - 1; i++) {
-
-        int min_idx = i;
-        for (int j = i + 1; j < N; j++) {
-            if (arr[j] < arr[min_idx]) {
-                min_idx = j;
-            }
-        }
-
-        int temp = arr[min_idx];
-        arr[min_idx] = arr[i];
-        arr[i] = temp;
-    }
-}
 
 int main() {
     printf("Enter the size of array : ");
     int n;
     scanf("%d",&n);
+
+    printf("Enter array elements : ");
+
     int arr[n];
-    printf("Enter the elements of array : ");
     for(int i = 0; i < n; i++) {
         scanf("%d",&arr[i]);
     }
 
-    selectionSort(arr, n);
+    for(int i = 0; i < n - 1; i++) {
+        // we find minimum index
 
-    printf("Sorted array: \n");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+        int min = i;
+        for(int j = i + 1; j < n; j++) {
+            if(arr[min] > arr[j]) {
+                min = j;
+            }
+        }
+
+        // we swap the minimum with the first value of that iteration
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
     }
-    printf("\n");
-    return 0;
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ",arr[i]);
+    }
 }
